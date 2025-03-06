42c5ef800bfc5ac1269981e7333516a02d5b4b63
Kaya Woods <kayanevada@gmail.com>
2025-03-06 09:31:47 -0800

    first function created

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,10 +1,38 @@
 import { useState } from 'react';
 const Bookshelf = () => {
+
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+      ]); 
+    const [newBook, setNewBook] = useState ({
+        title: '',
+        author: '',
+})
+
+ const handleInputChange = (event) => {
+    setNewBook({...newBook, [event.target.name]: event.target.value})
+ }   
+
 return (
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
-    {/* Form will go here */}
+    form onSubmit={handleInputChange}>
+        <label htmlFor="title">Title: </label>
+        <input
+          id="title"
+          name="title"
+          value={newBook.title}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          />
   </div>
   <div className="bookCardsDiv">{/* Book cards will display here */}</div>
 </div> 
