6d42724b514f22b9f4c27727ccfe9b7c4f5c141c
Kaya Woods <kayanevada@gmail.com>
2025-03-06 08:47:36 -0800

    setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 // src/App.jsx
 
+// src/App.jsx
+import './App.css';
+import Bookshelf from './components/Bookshelf/Bookshelf';
+
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+   <Bookshelf/>
+    </>
+  );
 };
 
 export default App;
+
+// line 12 to see files
